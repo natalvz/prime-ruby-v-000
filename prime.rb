6abbc24a6b85cf138bded79df91f8ae3d0999681
Natalia Castro <nat.alvz@gmail.com>
2018-05-19 23:40:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,14 @@
 # Add  code here!
 def prime?(n)
-    if n == 2 || n == 3
+    if n < 1 
+      false
+      elsif n == 2 || n == 3
       true
       elsif n % 2 == 0 || n % 3 == 0
       false
     else n.odd?
       true
+      
 end
   
 end
