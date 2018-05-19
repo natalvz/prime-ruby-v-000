449c2da49600a925544802b9513d0bd044005ad0
Natalia Castro <nat.alvz@gmail.com>
2018-05-19 23:26:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,1 @@
 # Add  code here!
-def prime?(n)
-  if n ≤ 1
-        return false
-     else if n ≤ 3
-        return true
-     else if n mod 2 = 0 or n mod 3 = 0
-        return false
-     let i ← 5
-     while i * i ≤ n
-        if n mod i = 0 or n mod (i + 2) = 0
-            return false
-        i ← i + 6
-     return true
-   end
-end
