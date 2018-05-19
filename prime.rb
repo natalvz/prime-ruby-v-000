484c8a63191f94e255d7678bb6915868bcd7057e
Natalia Castro <nat.alvz@gmail.com>
2018-05-19 23:36:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,10 +4,7 @@ def prime?(n)
       true
       else n % 2 == 0 || n % 3 == 0
       false
-    else while i * i <= n:
-        if n % i == 0:
-            return false
-    end
+    else n.odd?
 end
   
 end
