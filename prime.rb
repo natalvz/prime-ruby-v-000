dcd3386eea3f91f1fd1281cb30413adc91cc29b8
Natalia Castro <nat.alvz@gmail.com>
2018-05-19 23:35:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(n)
     if n == 2 || n == 3
       true
-      elsif n % 2 == 0 || n % 3 == 0
+      else n % 2 == 0 || n % 3 == 0
       false
     else while i * i <= n:
         if n % i == 0:
